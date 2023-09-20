1579d453b7a1eb5933b6b086b317fd955ee01291
bh1ff <ahmedayad1111@gmail.com>
2023-09-20 19:24:55 +0100

    function retrieve added

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,3 +1,19 @@
-// js test 1
-console.log("logic")
-console.log("hi")
+// API REF 
+const apiKey = "0b26a0d735f1c68e879212c2650e5b40"; // Your OpenWeatherMap API key
+const weatherBaseURL = "https://api.openweathermap.org/data/2.5/";
+
+// weather city function 
+function getCurrentWeather(cityName) {
+    let queryURL = `${weatherBaseURL}weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+
+    fetch(queryURL)
+        .then(response => response.json())
+        .then(data => {
+            // Handle the data here or pass it to another function
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("Error fetching current weather:", error);
+        });
+}
+
