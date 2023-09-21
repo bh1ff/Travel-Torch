220f13f47b2d4991c6b54e68323ce38060416c3a
bh1ff <ahmedayad1111@gmail.com>
2023-09-21 18:05:22 +0100

    new test fingers crossed

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -245,7 +245,6 @@ function getTopAttractions(cityName, lat, lon) {
         imagePlaceholder.className = "attractionImage"; // Class for the image of each attraction
         infoDiv.appendChild(imagePlaceholder);
 
-        getTopImageForAttraction(attractionNames);
       });
 
       console.log("Attraction Names Stored:", attractionNames);
@@ -257,20 +256,29 @@ function getTopAttractions(cityName, lat, lon) {
     });
 }
 
-
-// testing the new method of retrieval 
-function getTopImageForAttraction(attractionName) {
+// API TEST FOR SERP API JS 
+function testSerpApi() {
   const apiKey = "5180407f16258b8bd7959d7b76b1ba80af14fef715eb49a4107c149196e4e19b";
-  const query = encodeURIComponent(attractionName);
+  const query = encodeURIComponent("apple");
   const url = `https://serpapi.com/search.json?q=${query}&engine=google_images&ijn=0&api_key=${apiKey}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
       const imageUrl = data.images_results[0].link;
-      // Update your image src here
+      const footer = document.querySelector("footer");
+      const imgElement = document.createElement("img");
+
+      imgElement.src = imageUrl;
+      imgElement.alt = "Test Image";
+      imgElement.width = 200; // You can set the dimensions as you like
+
+      footer.appendChild(imgElement);
     })
     .catch(error => {
       console.error('Error fetching image:', error);
     });
 }
+
+// Call the function to test
+testSerpApi();
