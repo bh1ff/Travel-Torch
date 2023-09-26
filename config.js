45c0e2c2d8d298fb33890645621dbd7ef51cad58
bh1ff <ahmedayad1111@gmail.com>
2023-09-26 20:41:12 +0100

    new changes

---
+++ config.js
@@ -0,0 +1,9 @@
+const  googleAPIKey = 'AIzaSyDUT0XknlL2dbH-eGlu_tPQvs7xh_tMb48';
+const OTMAPI = "5ae2e3f221c38a28845f05b66a252504e753f805146378d6cae9fabd";
+const apiKey = "0b26a0d735f1c68e879212c2650e5b40";
+export {  
+    googleAPIKey,
+    OTMAPI,
+    apiKey,
+
+};
