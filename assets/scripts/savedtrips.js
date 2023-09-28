44d4fb970c9d377152c6b6305d29b6884aab6fbb
bh1ff <ahmedayad1111@gmail.com>
2023-09-28 15:36:08 +0100

    new logic to test

--- assets/scripts/savedtrips.js
+++ assets/scripts/savedtrips.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Add event listener to the "View trip" button
-    const viewTripButton = document.querySelector("#viewSaved");
+    const viewTripButton = document.querySelector("[data-bs-target='#tripModal']");
     if (viewTripButton) {
         viewTripButton.addEventListener("click", displaySavedTripDetails);
     }
@@ -8,16 +8,57 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function displaySavedTripDetails() {
     // Retrieve the saved trip details from localStorage
-    const savedTrip = JSON.parse(localStorage.getItem("currentTrip"));
+    const currentWeather = JSON.parse(localStorage.getItem("currentWeather"));
+    const fiveDayForecast = JSON.parse(localStorage.getItem("fiveDayForecast"));
+    const topAttractions = JSON.parse(localStorage.getItem("topAttractions"));
 
-    if (savedTrip) {
+    if (currentWeather && fiveDayForecast && topAttractions) {
         // Populate the modal with the saved trip details
-        const modalTitle = document.getElementById("staticBackdropLabel");
         const modalBody = document.querySelector(".modal-body");
+        modalBody.innerHTML = ""; // Clear previous content
 
-        modalTitle.textContent = savedTrip.city || "Unknown City"; // Replace with the saved city name
-        modalBody.textContent = savedTrip.details || "No details available"; // Replace with the saved trip details
+        // Display current weather
+        const currentWeatherDiv = document.createElement("div");
+        currentWeatherDiv.innerHTML = `
+            <h2 class="city-name">${currentWeather.name}</h2>
+            <p class="current-temperature">Temperature: ${currentWeather.main.temp}°C</p>
+            <p class="current-humidity">Humidity: ${currentWeather.main.humidity}%</p>
+            <p class="current-wind-speed">Wind Speed: ${currentWeather.wind.speed} MPH</p>
+        `;
+        modalBody.appendChild(currentWeatherDiv);
+
+        // Display 5-day forecast
+        const forecastDiv = document.createElement("div");
+        forecastDiv.id = "fiveDayForecast";
+        forecastDiv.classList.add("forecast-div");
+        fiveDayForecast.list.forEach((dayData, index) => {
+            if (index % 8 === 0) {
+                const dayDiv = document.createElement("div");
+                dayDiv.classList.add("forecast-day");
+                dayDiv.innerHTML = `
+                    <h4 class="forecast-date">${new Date(dayData.dt_txt).toLocaleDateString()}</h4>
+                    <img class="forecast-icon" src="https://openweathermap.org/img/w/${dayData.weather[0].icon}.png" alt="${dayData.weather[0].description}">
+                    <p class="forecast-temperature">Temp: ${dayData.main.temp}°C</p>
+                    <p class="forecast-humidity">Humidity: ${dayData.main.humidity}%</p>
+                `;
+                forecastDiv.appendChild(dayDiv);
+            }
+        });
+        modalBody.appendChild(forecastDiv);
+
+        // Display top attractions
+        const attractionsDiv = document.createElement("div");
+        attractionsDiv.innerHTML = `<h1 id="attractionsTitle">Top Attractions in ${currentWeather.name}</h1>`;
+        topAttractions.forEach(attraction => {
+            const attractionElement = document.createElement("div");
+            attractionElement.innerHTML = `
+                <h2 class="attractionName">${attraction.title}</h2>
+                <img class="attractionImage" src="${attraction.image}" alt="${attraction.title}">
+            `;
+            attractionsDiv.appendChild(attractionElement);
+        });
+        modalBody.appendChild(attractionsDiv);
     } else {
-        console.log("No saved trip found.");
+        console.log("No saved trip details found.");
     }
 }
